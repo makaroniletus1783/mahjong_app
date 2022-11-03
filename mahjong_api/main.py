07012bfad7fa13aea72d4e1fb4d07971687a043b
Yuki Inaba <daoyeyoushu@gmail.com>
2022-11-04 01:25:50 +0900

    画像のレスポンスを確認

--- mahjong_api/main.py
+++ mahjong_api/main.py
@@ -1,3 +1,6 @@
+import cv2
+import requests
+import os
 from importlib.resources import path
 from fastapi import FastAPI
 from pydantic import BaseModel  # リクエストbodyを定義するために必要
@@ -14,7 +17,19 @@ class Image(BaseModel):
 # シンプルなJSON Bodyの受け取り
 @app.post("/mahjong/predict")
 # 上で定義したUserモデルのリクエストbodyをuserで受け取る
-# image = {"path": "contents/inu.jpg"}
+# image = {"image_url": "contents/inu.jpg"}
 def create_image(image: Image):
+    url = image.image_url
+    response = requests.get(url)
+    im = response.content
+
+    file_name = "test.jpg"
+    with open(file_name, "wb") as f:
+        f.write(im)
+
+    # 以下で画像の推論
+
+    os.remove(file_name)
+
     # レスポンスbody
-    return {"res": "ok", "画像": image.image_url}
+    return {"res": "ok"}
